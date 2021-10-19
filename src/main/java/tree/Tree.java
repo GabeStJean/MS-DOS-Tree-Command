074635ea2bcdf8cf099d821066fe5ef3a702ce2e
Gabriel St Jean <gjs19225@uga.edu>
2021-10-19 10:51:07 -0400

    Created the recursive definition for listing files in the print tree method

--- src/main/java/tree/Tree.java
+++ src/main/java/tree/Tree.java
@@ -15,15 +15,30 @@ public class Tree {
     /**
      * Ouputs files within a subdirecty in a hiearchial fashion.
      * @param file the file object to list all sub directories
+     * @param level the level of the root file in the print tree
      */
-    public static void printTree(File file) {
+    public static void printTree(File file, int level) {
+        if (file.exists() == false) {
+            System.out.println("find: " + file.getName() +  " : No such file or directory");
+            return;
+        } // if
 
+        // Base case: files is not a directory
+        if (file.isDirectory() == false) {
+            System.out.println(level + " " + file.getName());
+            return;
+        } // if
+
+        // Recursive case
+        System.out.println(level + " " +  file.getName());
+        Arrays.stream(file.listFiles()).forEach(e -> printTree(e, level + 1));
     } // printTree
 
     /**
      * The entry point into the program.
      */
     public static void main(String[] args)  {
+        // Use the current directory if no args are specified
         if (args.length == 0) {
             args = new String[] {"."};
         } // if
@@ -31,5 +46,9 @@ public class Tree {
         // Debugging: Check if args work correctly
         String argInput = Arrays.stream(args).collect(Collectors.joining(", ", "[", "]"));
         System.out.println("args: " + argInput);
+
+        // Display the subdirectories of all arguments specified
+        Arrays.stream(args).map(e -> new File(e))
+            .forEach(e -> printTree(e, 0));
     } // main
 } // Tree
